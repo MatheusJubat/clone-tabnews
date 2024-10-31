4c0fd2affb91d323b6c823e35b297909c5f1776c
MatheusJubat <79995232+MatheusJubat@users.noreply.github.com>
2024-10-31 12:18:41 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>O Gabrielzinh chupa pau</h1>
+function Home() {
+  return <h1>O Gabrielzinh chupa pau</h1>;
 }
 
+function teste() {}
+console.log("teste");
 export default Home;
+
+function teste2() {
+  console.log("errado");
+}
